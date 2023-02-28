03ec6cff405c7a2911f481a871ac0c795967e7aa
yusunqi <ysuqi01@126.com>
2023-02-28 22:00:45 +0800

    feat: 新增项目经历内容

--- src/views/project/data.js
+++ src/views/project/data.js
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import useImage from './img'
 
 export default function useProject() {
-  const { salesInages, dmpImages } = useImage()
+  const { salesInages, dmpImages, yzImages } = useImage()
   const list = ref([
     {
       id: '1',
@@ -39,6 +39,84 @@ export default function useProject() {
           ],
           projectUrl: '',
           projectImgages: dmpImages
+        }
+      ]
+    },
+    {
+      id: '2',
+      company: '弥驰祥科技有限公司',
+      projects: [
+        {
+          id: '2-1',
+          projectName: '区块驿站APP',
+          description: '（2021-07 ～ 2021-09， 4人， 前端独立开发， 移动端）',
+          technologys: ['uni-app', 'uViewUi'],
+          projectDescription: '一款以比特币为核心内容的论坛App',
+          projectResponsibility: [
+            '1、负责项目的搭建（从0到1）和项目的规范化制作',
+            '2、负责文章发布、评论、知识付费、实名认证、收藏点赞、会员充值等功能项20+',
+            '3、封装公共的业务组件，组件化开发和公共方法抽离，减少单文件的代码量，以便于组件的维护和复用'
+          ],
+          projectUrl: '',
+          projectImgages: yzImages
+        }
+      ]
+    },
+    {
+      id: '3',
+      company: '博丰物联科技有限公司',
+      projects: [
+        {
+          id: '3-1',
+          projectName: '城市智能井盖后台管理系统',
+          description: '（2020-11 ～ 2021-01， 3人， 前端独立开发）',
+          technologys: ['Angular', 'TypeScript', 'NG-ALAIN', 'NG-ZORRO'],
+          projectDescription: '用于管理城市井盖的维护和实时监测',
+          projectResponsibility: [
+            '1、负责项目系统重构（从0到1），实现前后端分离开发',
+            '2、负责角色、人员、机构、井盖、水位计、报警设备管理模块功能开发（完成功能项15+）',
+            '3、结合高德地图完成井盖的定位和井盖的警报状态，便于维修人员及时维修',
+            '4、结合webSocket实时获取水位计设备的报警通知',
+            '5、对大文件进行组件拆分以及公共方法的抽取，减少重复代码及方法复用'
+          ],
+          projectUrl: '',
+          projectImgages: []
+        }
+      ]
+    },
+    {
+      id: '4',
+      company: '嗨知了科技有限公司',
+      projects: [
+        {
+          id: '4-1',
+          projectName: '酒店预定后台管理系统',
+          description: '（2020-04 ～ 2021-05， 9人， 前端开发负责人）',
+          technologys: ['Vue系列', 'ElementUI'],
+          projectDescription:
+            '该系统为酒店提供了酒店预定管理和服务管理（二次开发）',
+          projectResponsibility: [
+            '1、规划项目功能模块的计划及模块开发的分配工作（1名实习生）',
+            '2、负责产品、订单、客房等管理模块功能的开发',
+            '3、封装公共的业务组件，组件化开发和公共方法抽离，减少单文件的代码量，以便于组件的维护和复用'
+          ],
+          projectUrl: '',
+          projectImgages: []
+        },
+        {
+          id: '4-2',
+          projectName: '知了酒店管理小程序',
+          description: '（2020-06 ～ 2021-07， 9人， 前端开发负责人）',
+          technologys: ['微信小程序', 'VantUI'],
+          projectDescription:
+            '为酒店提供各方面的服务如：WIFI密码、酒店商城等（微信小程序原生开发）',
+          projectResponsibility: [
+            '1、规划项目功能模块的计划及模块开发的分配工作（1名实习生）',
+            '2、负责扫描下单、客房服务、wifi服务等管理模块功能的开发',
+            '3、封装公共的业务组件，组件化开发和公共方法抽离，减少单文件的代码量，以便于组件的维护和复用'
+          ],
+          projectUrl: '',
+          projectImgages: []
         }
       ]
     }
--- src/views/project/img.js
+++ src/views/project/img.js
@@ -11,13 +11,24 @@ import dpm5 from '@/assets/img/dpm/dpm5.png'
 import dpm6 from '@/assets/img/dpm/dpm6.png'
 import dpm7 from '@/assets/img/dpm/dpm7.png'
 import dpm8 from '@/assets/img/dpm/dpm8.png'
+import yz1 from '@/assets/img/yz/yz1.png'
+import yz2 from '@/assets/img/yz/yz2.png'
+import yz3 from '@/assets/img/yz/yz3.png'
+import yz4 from '@/assets/img/yz/yz4.png'
+import yz5 from '@/assets/img/yz/yz5.png'
+import yz6 from '@/assets/img/yz/yz6.png'
+import yz7 from '@/assets/img/yz/yz7.png'
+import yz8 from '@/assets/img/yz/yz8.png'
+import yz9 from '@/assets/img/yz/yz9.png'
 
 import { ref } from 'vue'
 export default function useImage() {
   const salesInages = ref([sales1, sales2, sales3, sales4, sales5])
   const dmpImages = ref([dpm1, dpm2, dpm3, dpm4, dpm5, dpm6, dpm7, dpm8])
+  const yzImages = ref([yz1, yz2, yz3, yz4, yz5, yz6, yz7, yz8, yz9])
 
   return {
+    yzImages,
     salesInages,
     dmpImages
   }
