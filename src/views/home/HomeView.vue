92fb3679bec76e3b3327e5811caf0a88d5a98f7e
yusunqi <ysuqi01@126.com>
2023-04-10 21:04:41 +0800

    feat: 新增个人优势和个人简介

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,30 @@
 <template>
   <div>
     <InfoView />
+    <van-cell-group inset>
+      <van-cell title="个人优势">
+        <template #label>
+          <ol>
+            <li v-for="a in advantage" :key="a.id">
+              {{ a.id }}、{{ a.title }}
+            </li>
+          </ol>
+        </template>
+      </van-cell>
+      <van-cell title="个人简介">
+        <template #label>
+          <ol>
+            <li v-for="int in introduction" :key="int.id">
+              {{ int.id }}、{{ int.title }}
+            </li>
+          </ol>
+        </template>
+      </van-cell>
+    </van-cell-group>
   </div>
 </template>
 
 <script setup>
 import InfoView from './InfoView.vue'
+import { advantage, introduction } from './data'
 </script>
---
+++ src/views/home/data.js
@@ -0,0 +1,40 @@
+const advantage = [
+  {
+    id: 1,
+    title: '熟练掌握HTML5、CSS、Sass、Less、JavaScript、ES6、TypeScript等语言'
+  },
+  {
+    id: 2,
+    title: '熟练掌握Vue(项目10+)、React、Angular、UinApp、微信小程序等款架'
+  },
+  {
+    id: 3,
+    title: '熟悉Git、webpack、rollup、Vite等工具'
+  },
+  {
+    id: 4,
+    title: '了解Nodejs、MySQL数据等基本使用'
+  },
+  {
+    id: 5,
+    title: '常用的组件ElementUI、VantUI、AntDesign'
+  }
+]
+
+const introduction = [
+  { id: 1, title: '性格：温和、平易近人、容易相处' },
+  {
+    id: 2,
+    title:
+      '优点：爱学习，平常空闲时间喜欢去一些网站（比如：bilibili、稀土掘金、MDN等等）学习一些新的技术和研究一些框架的源码以及在稀土掘金上发表一些文章（总结自己的一些学习的内容）'
+  },
+  { id: 3, title: '缺点：学历不高' },
+  { id: 4, title: '稀土掘金主页：https://ysuqi.gitee.io/fish-dev/' },
+  {
+    id: 5,
+    title:
+      '个人习惯：开发设备：MacBook Pro；软件和平台：开发工具：VSCode/Webstorm/HbuilderX/Postmat/Apifox等等；写作工具：Typora/wolai/无边际/XMind/ProcessOn等等；项目协作平台：蓝湖、Gitee、Tapd等等；'
+  }
+]
+
+export { advantage, introduction }
